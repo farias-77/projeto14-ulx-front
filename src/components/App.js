a3db6a219b1540ac73fbc3ee2cfbe4c0bba44446
farias_77 <gabrielfalves2003@gmail.com>
2022-07-06 15:01:07 -0300

    cria header página

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home.js";
+import Header from "./Header/Header.js";
 
 export default function App(){
 
     return (
 
         <BrowserRouter>
+            <Header />
             <Routes>
-                
+                <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export default function Home(){
+
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    margin-top: 150px;
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+
+`;
