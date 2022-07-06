8be30d4483627115ff2d6137123f2dffed771348
farias_77 <gabrielfalves2003@gmail.com>
2022-07-06 14:43:10 -0300

    cria setup front

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "./components/App.js";
+import ReactDOM from "react-dom";
+import "./assets/reset.css";
+import "./assets/styles.css";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
