b092f10e09bf9cce3894a30d36f36b2e891d6f1f
farias_77 <gabrielfalves2003@gmail.com>
2022-07-06 15:14:19 -0300

    cria layout produto na página inicial

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Product from "./Product.js";
 
 export default function Home(){
 
     return (
         <Container>
-
+            <Products>
+                <Product image='https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg' name='Gabriel Barbosa' description='O melhor atacante do brasil hoje tem gol do gabigol' price='90M'/>
+            </Products>
         </Container>
     )
 }
@@ -17,5 +20,9 @@ const Container = styled.div`
     position: fixed;
     top: 0;
     bottom: 0;
+    background-color: #eeeeee;
+`;
 
+const Products = styled.div`
+    
 `;
