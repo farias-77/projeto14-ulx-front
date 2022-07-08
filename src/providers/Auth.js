38b51ee956e7b219bc56e82302e489c5eb0552eb
vcsil <vcsilva@poli.ufrj.br>
2022-07-07 22:56:14 -0300

    Cria um contexto
    Para persistência de login e repassar dados entre componentes

---
+++ src/providers/Auth.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+export const AuthContext = React.createContext({});
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        token: "",
+        entrou: false,
+    });
+    useEffect(() => {
+        const usuario = localStorage.getItem("usuario");
+        if (usuario) {
+            const objetoUsuario = JSON.parse(usuario);
+            setUser({
+                ...user,
+                name: objetoUsuario.name,
+                email: objetoUsuario.email,
+                token: objetoUsuario.token,
+                entrou: objetoUsuario.entrou,
+            });
+        }
+    }, []);
+
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    const usuario = { user, setUser };
+
+    return (
+        <AuthContext.Provider value={usuario}>{children}</AuthContext.Provider>
+    );
+}
