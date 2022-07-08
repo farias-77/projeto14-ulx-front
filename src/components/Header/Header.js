1fd7676551a92025362a85102a7bb76daebf8436
vcsil <vcsilva@poli.ufrj.br>
2022-07-08 01:44:47 -0300

    Adiciona link para login e para sair.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,53 @@
+import { useNavigate, Link } from "react-router-dom";
 import styled from "styled-components";
+import { useContext } from "react";
 
+import { AuthContext } from "../../providers/Auth.js";
 import logo from "../../assets/image/logo.png";
 
-export default function Header() {
+export default function Header({ entrou }) {
+    const navigate = useNavigate();
+
+    const { user, setUser } = useContext(AuthContext);
+
+    function Sair() {
+        setUser({ ...user, name: "", email: "", token: "", entrou: false });
+        localStorage.removeItem("usuario");
+        navigate("/");
+    }
+
     return (
-        <Container>
+        <Container entrou={entrou}>
             <Logo>
                 <img src={logo} alt="logo" />
                 <p>O seu marketplace de confiança</p>
             </Logo>
+            <Saida>
+                <ion-icon
+                    name="exit-outline"
+                    style={{
+                        width: "28px",
+                        height: "28px",
+                        color: "black",
+                        cursor: "pointer",
+                    }}
+                    onClick={() => Sair()}
+                />
+            </Saida>
             <Icons>
                 <ion-icon name="home" />
                 <ion-icon name="storefront" />
                 <ion-icon name="cart" />
-                <ion-icon name="person" />
+                <Link to="/sign-in">
+                    <ion-icon name="person" />
+                </Link>
             </Icons>
         </Container>
     );
 }
 
 const Container = styled.div`
+    display: ${(props) => (props.entrou ? "initial" : "var(--display-none)")};
     width: 100%;
     position: fixed;
     top: 0;
@@ -68,4 +96,10 @@ const Icons = styled.div`
         font-size: 30px;
         color: white;
     }
+`;
+
+const Saida = styled.div`
+    position: fixed;
+    right: 24px;
+    top: 24px;
 `;
