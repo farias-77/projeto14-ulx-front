57150424460b3a6711e98320a2b4038cd7b56fac
vcsil <vcsilva@poli.ufrj.br>
2022-07-08 01:44:28 -0300

    Ativa o header quando entra no home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,110 @@
+import { useContext, useEffect } from "react";
 import styled from "styled-components";
 import Product from "./Product.js";
 
-export default function Home(){
+import { AuthContext } from "../../providers/Auth.js";
+
+export default function Home() {
+    const { user, setUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        setUser({
+            ...user,
+            entrou: true,
+        });
+    }, []);
 
     return (
         <Container>
             <Category>
                 <h1>Jogadores: </h1>
                 <Products>
-                    <Product image='https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg' name='Gabriel Barbosa' description='O melhor atacante do brasil hoje tem gol do gabigol' price='90M'/>
-                    <Product image='https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg' name='Gabriel Barbosa' description='O melhor atacante do brasil hoje tem gol do gabigol' price='90M'/>
-                    <Product image='https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg' name='Gabriel Barbosa' description='O melhor atacante do brasil hoje tem gol do gabigol' price='90M'/>
-                    <Product image='https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg' name='Gabriel Barbosa' description='O melhor atacante do brasil hoje tem gol do gabigol' price='90M'/>
+                    <Product
+                        image="https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg"
+                        name="Gabriel Barbosa"
+                        description="O melhor atacante do brasil hoje tem gol do gabigol"
+                        price="90M"
+                    />
+                    <Product
+                        image="https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg"
+                        name="Gabriel Barbosa"
+                        description="O melhor atacante do brasil hoje tem gol do gabigol"
+                        price="90M"
+                    />
+                    <Product
+                        image="https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg"
+                        name="Gabriel Barbosa"
+                        description="O melhor atacante do brasil hoje tem gol do gabigol"
+                        price="90M"
+                    />
+                    <Product
+                        image="https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg"
+                        name="Gabriel Barbosa"
+                        description="O melhor atacante do brasil hoje tem gol do gabigol"
+                        price="90M"
+                    />
                 </Products>
             </Category>
             <Category>
                 <h1>Times: </h1>
                 <Products>
-                    <Product image='https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90' name='Flamengo' description='O melhor time do brasil hoje tem gol do gabigol' price=''/>
-                    <Product image='https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90' name='Flamengo' description='O melhor time do brasil hoje tem gol do gabigol' price=''/>
-                    <Product image='https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90' name='Flamengo' description='O melhor time do brasil hoje tem gol do gabigol' price=''/>
-                    <Product image='https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90' name='Flamengo' description='O melhor time do brasil hoje tem gol do gabigol' price=''/>
+                    <Product
+                        image="https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90"
+                        name="Flamengo"
+                        description="O melhor time do brasil hoje tem gol do gabigol"
+                        price=""
+                    />
+                    <Product
+                        image="https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90"
+                        name="Flamengo"
+                        description="O melhor time do brasil hoje tem gol do gabigol"
+                        price=""
+                    />
+                    <Product
+                        image="https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90"
+                        name="Flamengo"
+                        description="O melhor time do brasil hoje tem gol do gabigol"
+                        price=""
+                    />
+                    <Product
+                        image="https://play-lh.googleusercontent.com/9EuLX3VSldq04CPSZ2s6aL-SG1VDTFCtLg9ng3d2bhJofpaN4mZmEQqsqdrmah92r90"
+                        name="Flamengo"
+                        description="O melhor time do brasil hoje tem gol do gabigol"
+                        price=""
+                    />
                 </Products>
             </Category>
             <Category>
                 <h1>Estádios: </h1>
                 <Products>
-                    <Product image='https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg' name='Maracanã' description='gigante esse é gigante' price=''/>
-                    <Product image='https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg' name='Maracanã' description='gigante esse é gigante' price=''/>
-                    <Product image='https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg' name='Maracanã' description='gigante esse é gigante' price=''/>
-                    <Product image='https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg' name='Maracanã' description='gigante esse é gigante' price=''/>
+                    <Product
+                        image="https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg"
+                        name="Maracanã"
+                        description="gigante esse é gigante"
+                        price=""
+                    />
+                    <Product
+                        image="https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg"
+                        name="Maracanã"
+                        description="gigante esse é gigante"
+                        price=""
+                    />
+                    <Product
+                        image="https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg"
+                        name="Maracanã"
+                        description="gigante esse é gigante"
+                        price=""
+                    />
+                    <Product
+                        image="https://flamengorj.com.br/arquivos/noticia/9f936923-468a-4128-832a-615636170f43_conteudo.jpg"
+                        name="Maracanã"
+                        description="gigante esse é gigante"
+                        price=""
+                    />
                 </Products>
             </Category>
         </Container>
-    )
+    );
 }
 
 const Container = styled.div`
@@ -49,15 +120,15 @@ const Products = styled.div`
     width: 100%;
     height: 216px;
     overflow-y: scroll;
-    
+
     display: flex;
 `;
 
 const Category = styled.div`
     margin-top: 20px;
 
-    h1 {        
-        font-family: 'Roboto';
+    h1 {
+        font-family: "Roboto";
         font-style: normal;
         font-weight: 700;
         font-size: 26px;
