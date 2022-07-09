f1f49b4cf99432a991ba5c4cb24d61e47a8659e7
vcsil <vcsilva@poli.ufrj.br>
2022-07-09 16:25:38 -0300

    Comenta partes com console log

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,23 +1,22 @@
 import styled from "styled-components";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Bars } from "react-loader-spinner";
 
 export default function ProductPage() {
-    
-    const productId = useParams();
-    const userInfo = localStorage.getItem("usuario")
+    // const productId = useParams();
+    // const userInfo = localStorage.getItem("usuario");
     // inicializar vazio para preencher com dados do db
-    const [ product, setProduct ] = useState({ 
+    const [product, setProduct] = useState({
         name: "Gabriel Barbosa",
         price: "90M",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut justo orci, ornare et enim id, hendrerit condimentum est. Mauris sit amet condimentum ante. Fusce molestie posuere eleifend. Sed facilisis massa et diam mattis rhoncus. Sed bibendum urna et lacus ultricies mollis. Vestibulum fermentum sed massa non finibus. Morbi tempor sed arcu sollicitudin suscipit. Nunc eleifend lorem eu nisi lobortis, vitae ullamcorper urna gravida. Etiam pharetra pellentesque molestie. Fusce ut ipsum nec mauris maximus rutrum. Vestibulum mauris nisi, euismod vel efficitur id, auctor vitae tortor. Phasellus tincidunt aliquet porta. Nulla suscipit ac dolor id egestas. Proin hendrerit, turpis non ullamcorper vehicula, leo dolor vulputate nisi, quis tincidunt dui velit quis sapien",
-        url: "https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg"
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut justo orci, ornare et enim id, hendrerit condimentum est. Mauris sit amet condimentum ante. Fusce molestie posuere eleifend. Sed facilisis massa et diam mattis rhoncus. Sed bibendum urna et lacus ultricies mollis. Vestibulum fermentum sed massa non finibus. Morbi tempor sed arcu sollicitudin suscipit. Nunc eleifend lorem eu nisi lobortis, vitae ullamcorper urna gravida. Etiam pharetra pellentesque molestie. Fusce ut ipsum nec mauris maximus rutrum. Vestibulum mauris nisi, euismod vel efficitur id, auctor vitae tortor. Phasellus tincidunt aliquet porta. Nulla suscipit ac dolor id egestas. Proin hendrerit, turpis non ullamcorper vehicula, leo dolor vulputate nisi, quis tincidunt dui velit quis sapien",
+        url: "https://vejario.abril.com.br/wp-content/uploads/2021/09/gabi-gol-globo.jpg.jpg",
     });
     const [carregando, setCarregando] = useState(false);
 
     useEffect(() => {
-
         // const URL = `https://ulx-store.herokuapp.com/product/${productId}`;
         // const config = {
         //     headers: {
@@ -27,45 +26,47 @@ export default function ProductPage() {
         // const promise = axios.get(URL, config);
         //  atualizar com dados do db
         // promise.then(() => {
-        //     setProduct({...product}); 
+        //     setProduct({...product});
         // })
-        setProduct({...product});
-        console.log(userInfo)
-        console.log(productId)
+        setProduct({ ...product });
+        // console.log(userInfo)
+        // console.log(productId)
     }, []);
-    
-    function addCart(){
-        
-        // lógica para add ao carrinho 
-        
-        setCarregando(true);
-        setTimeout(() => {setCarregando(false)}, 4000);
-    }
 
+    function addCart() {
+        // lógica para add ao carrinho
+
+        setCarregando(true);
+        setTimeout(() => {
+            setCarregando(false);
+        }, 4000);
+    }
 
     return (
         <Container>
             <img
-                src={ product.url ? product.url : "loading..." }
+                src={product.url ? product.url : "loading..."}
                 alt="imagem produto"
             />
             <ProductInfo>
-                <h2>{ product.name ? product.name : "loading..." }</h2>
-                <h4>{ product.price ? product.price : "loading..." }</h4>
-                <h3>{ product.description ? product.description : "loading..."}</h3>
+                <h2>{product.name ? product.name : "loading..."}</h2>
+                <h4>{product.price ? product.price : "loading..."}</h4>
+                <h3>
+                    {product.description ? product.description : "loading..."}
+                </h3>
             </ProductInfo>
-            <Botao onClick={() => addCart() }>
-                    {carregando ? (
-                        <Bars
-                            height="40"
-                            width="40"
-                            color="white"
-                            ariaLabel="loading"
-                        />
-                    ) : (
-                        <p>Adicionar ao carrinho</p>
-                    )}
-                </Botao>
+            <Botao onClick={() => addCart()}>
+                {carregando ? (
+                    <Bars
+                        height="40"
+                        width="40"
+                        color="white"
+                        ariaLabel="loading"
+                    />
+                ) : (
+                    <p>Adicionar ao carrinho</p>
+                )}
+            </Botao>
         </Container>
     );
 }
@@ -92,7 +93,7 @@ const Container = styled.div`
 
 const ProductInfo = styled.div`
     width: 100%;
-    
+
     margin-top: 30px;
 
     h2 {
@@ -126,8 +127,8 @@ const Botao = styled.button`
     justify-content: center;
     align-items: center;
 
-    p{
+    p {
         font-size: 20px;
-        color: #FFFFFF;
+        color: #ffffff;
     }
 `;
